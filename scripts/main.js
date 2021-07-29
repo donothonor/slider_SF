bb5957b65a7d76d287a9a8df2139213461d99767
VADIM <nagiozv2@gmail.com>
2021-07-29 17:41:08 +0400

    sad

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
 const images = document.querySelectorAll('.image-box__item')
+const rightArrow = document.querySelector('.content-arrow__right')
+const leftArrow = document.querySelector('.content-arrow__left')
+let i = 0
 
-console.log(images)
+rightArrow.addEventListener('click', moveForward)
+
+
+function moveForward () {
+    ++i
+    if(i >= images.length) {
+        images[i - 1].classList.remove('block')
+        i = 0
+        images[i].classList.add('block')
+    } else {
+        images[i - 1].classList.remove('block')
+        images[i].classList.add('block')
+    }
+}
